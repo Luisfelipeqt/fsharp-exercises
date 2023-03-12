a7a077fcf4c3b4cb868b19471ad7cccfbf0737ca
Luis Felipe Rodrigues <luisfelipebr1995@gmail.com>
2023-03-12 18:51:08 -0300

    Treinaweb

---
+++ Program.fs
@@ -0,0 +1,11 @@
+﻿open System
+
+[<EntryPoint>]
+
+
+let main argv =
+    printfn "Hello World from F#!"
+    printfn "Digite seu nome: "
+    let nome = Console.ReadLine()
+    printf "Seu nome é: %s\n" nome 
+    0
