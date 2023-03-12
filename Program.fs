e728030d1f94cdf89473514b61d605039b0834d3
Luis Felipe Rodrigues <luisfelipebr1995@gmail.com>
2023-03-12 20:04:39 -0300

    Criando Expressões(FUNÇÕES) e chamando a Expressão na main

--- Program.fs
+++ Program.fs
@@ -1,14 +1,18 @@
 ﻿open System
 
-[<EntryPoint>]
 
+let buildMessage (name:string): string =
+    if name.Length > 5 then
+        "Olá, " + name + ". Seu nome é longo"
+    else
+        "Olá, " + name + ". Seu nome é curto."
+    
+
+[<EntryPoint>]
 
 let main argv =
     printfn "Hello World from F#!"
     printfn "Digite seu nome: "
     let mutable nome:string = Console.ReadLine()
-    nome <- "Bem vindo meu amigo Felipe"
-    printf "Seu nome é: %s\n" nome
-  
-
+    printf "%s\n" (buildMessage nome)
     0
