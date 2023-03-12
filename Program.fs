563597e4f1bdfb283674d0666f6e691635e21921
Luis Felipe Rodrigues <luisfelipebr1995@gmail.com>
2023-03-12 19:30:37 -0300

    Definindo TIPOS explicitamente

--- Program.fs
+++ Program.fs
@@ -6,6 +6,9 @@
 let main argv =
     printfn "Hello World from F#!"
     printfn "Digite seu nome: "
-    let nome = Console.ReadLine()
-    printf "Seu nome é: %s\n" nome 
+    let mutable nome:string = Console.ReadLine()
+    nome <- "Bem vindo meu amigo Felipe"
+    printf "Seu nome é: %s\n" nome
+  
+
     0
